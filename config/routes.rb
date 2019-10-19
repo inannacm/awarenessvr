c51dd6650c0c195d44ba2b7154bc2aa1824a8651
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-10-19 23:16:12 +0100

    updated changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#about'
   get 'pages/contact'
   get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
